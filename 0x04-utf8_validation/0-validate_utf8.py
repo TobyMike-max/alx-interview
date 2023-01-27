d545cb3b2f9b74598e76f4756da31a0b9b3ff853
TobyMike-max <osinubi.michael@gmail.com>
2023-01-27 09:30:29 +0100

    [Modified]: Limit lines to a maximum of 79 characters

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,20 +14,21 @@ def validUTF8(data):
     Example:
         For this data to be utf-8 compliant it must follow a certain pattern
         Every single byte(8 bit) integer must start with 0, while,
-        Every multi byte(4 bytes most) would start with either of these headers:
+        Every multi byte(4 bytes most) starts with either of these headers:
         * 110 for 2 bytes
         * 1110 for 3 bytes
         * 11110 for 4 bytes
         Followed closely by 10 as header for the remaining bytes
         If a data doesn't comply with these pattern it isn't utf-8 compliant
         
-        data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+        data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 
+        111, 111, 108, 33]
         N.B Each integer reps 1 byte of data, therefore handle the 8 least
         significant bits of each integer.
-        This data above is utf-8 compliant because all numbers are single byte,
-        therefore all start with 0 to complete their 8bits and there are no encoded
-        bytes in the preceeding integer.
-        
+        This data above is utf-8 compliant because all numbers are single 
+        byte, therefore all start with 0 to complete their 8bits and there
+        are no encoded bytes in the preceeding integer.
+
         count variable just checks the number of encoded bytes in the
         preceeding integers for a character
     """
