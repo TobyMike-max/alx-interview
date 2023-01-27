76c408d45ef67003d5e41184468a1406be7ff44e
TobyMike-max <osinubi.michael@gmail.com>
2023-01-27 09:44:41 +0100

    [Modified]: PEP 8 standards implementation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,23 +10,6 @@ def validUTF8(data):
     Return:
         True if the data validates utf-8
         Otherwise False
-
-    Example:
-        For this data to be utf-8 compliant it must follow a certain pattern
-        Every single byte(8 bit) integer must start with 0, while,
-        Every multi byte(4 bytes most) starts with either of these headers:
-        * 110 for 2 bytes
-        * 1110 for 3 bytes
-        * 11110 for 4 bytes
-        Followed closely by 10 as header for the remaining bytes
-        If a data doesn't comply with these pattern it isn't utf-8 compliant
-        data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 
-        111, 111, 108, 33]
-        This data above is utf-8 compliant because all numbers are single 
-        byte, therefore all start with 0 to complete their 8bits and there
-        are no encoded bytes in the preceeding integer.
-        count variable just checks the number of encoded bytes in the
-        preceeding integers for a character
     """
 
     count = 0
